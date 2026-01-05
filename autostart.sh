871f0bb97d6323138844aa577dd6965fd09ff41c
ysl2 <songliyu97@gmail.com>
2026-01-05 16:16:15 +0800

    Add autostart localhost point.

--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 [ -f ~/.vocal/0/scripts/wm/autostart_wayland.sh ] && ~/.vocal/0/scripts/wm/autostart_wayland.sh
+
+[ -f ~/.config/sway/autostart.localhost.sh ] && ~/.config/sway/autostart.localhost.sh
